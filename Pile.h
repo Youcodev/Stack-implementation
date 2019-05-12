25b6c864bb99b2743659bbe36dbe64aaf3db27ff
Gergi <44761449+Youcodev@users.noreply.github.com>
2019-05-11 21:45:16 -0400

    Remove unnecessary comment

--- Pile.h
+++ Pile.h
@@ -4,11 +4,6 @@
 * Auteur: Gergi Younis
 *******************************************/
 
-/************************************************************************************************************
-* Une autre solution qui utilise un vector à la place d'un tableau dynamique est proposée (fichier vector). *
-* En fin de compte, un vector c'est aussi une forme de tableau dynamique !									*
-*************************************************************************************************************/
-
 #ifndef PILE_H
 #define PILE_H
 
@@ -17,7 +12,7 @@ using namespace std;
 template <typename T>
 class Pile {
 public:
-//Constructeur par défaut
+//Constructeur par dÃ©faut
 	Pile();
 
 //Destructeur
@@ -25,18 +20,18 @@ public:
 
 /*********************************************************************
 * Fonction : empiler												 *
-* Description : Ajoute un élément sur la pile (au sommet du tableau) *
-* Paramètres : Un élément de type générique à ajouter				 *
-* Valeur de retour : True si l'ajout est réussi						 *
+* Description : Ajoute un Ã©lÃ©ment sur la pile (au sommet du tableau) *
+* ParamÃ¨tres : Un Ã©lÃ©ment de type gÃ©nÃ©rique Ã  ajouter				 *
+* Valeur de retour : True si l'ajout est rÃ©ussi						 *
 *					 i.e. la pile n'est pas pleine					 *
 **********************************************************************/
 	bool empiler(const T&);
 
 /*****************************************************************************************
 * Fonction : depiler																	 *
-* Description : Retire le dernier élément ajouté à la pile et le retourne par paramètre  *
-* Paramètres : Un type générique pour le retour de l'élément du dessus de la pile        *
-* Valeur de retour : True si le dépilage est réussi										 *
+* Description : Retire le dernier Ã©lÃ©ment ajoutÃ© Ã  la pile et le retourne par paramÃ¨tre  *
+* ParamÃ¨tres : Un type gÃ©nÃ©rique pour le retour de l'Ã©lÃ©ment du dessus de la pile        *
+* Valeur de retour : True si le dÃ©pilage est rÃ©ussi										 *
 *					 i.e. la pile n'est pas vide										 *
 ******************************************************************************************/
 	bool depiler(T& elem);
@@ -44,7 +39,7 @@ public:
 /********************************************************
 * Fonction : estVide									*
 * Description : Indique si la pile est vide				*
-* Paramètres : Aucun									*
+* ParamÃ¨tres : Aucun									*
 * Valeur de retour : True si la pile est vide			*
 *********************************************************/
 	bool estVide() const;
@@ -52,31 +47,31 @@ public:
 /**********************************************************
 * Fonction : estPleine									  *
 * Description : Indique si la pile est pleine			  *
-* Paramètres : Aucun									  *
+* ParamÃ¨tres : Aucun									  *
 * Valeur de retour : True si la pile est pleine			  *
 ***********************************************************/
 	bool estPleine() const;
 
 /**********************************************************
 * Fonction : obtenirSommet								  *
-* Description : Accéder à l'élément du dessus de la pile  *
-* Paramètres : Aucun									  *
-* Valeur de retour : Référence sur cet élément			  *
+* Description : AccÃ©der Ã  l'Ã©lÃ©ment du dessus de la pile  *
+* ParamÃ¨tres : Aucun									  *
+* Valeur de retour : RÃ©fÃ©rence sur cet Ã©lÃ©ment			  *
 ***********************************************************/
 	const T& obtenirSommet() const;
 
 /**************************************************************
 * Fonction : obtenirTaille									  *
-* Description : Détermine le nombre d'éléments dans la pile	  *
-* Paramètres : Aucun										  *
-* Valeur de retour : Nombre d'éléments présents dans la pile  *
+* Description : DÃ©termine le nombre d'Ã©lÃ©ments dans la pile	  *
+* ParamÃ¨tres : Aucun										  *
+* Valeur de retour : Nombre d'Ã©lÃ©ments prÃ©sents dans la pile  *
 ***************************************************************/
 	int obtenirTaille() const;
 
 private:
-	int const capacite_; //La capacité du tableau restera fixe
-	int nombreElements_; //Le nombre d'éléments présents dans la pile
-	T* elements_; //Un tableau dynamique d'éléments génériques
+	int const capacite_; //La capacitÃ© du tableau restera fixe
+	int nombreElements_; //Le nombre d'Ã©lÃ©ments prÃ©sents dans la pile
+	T* elements_; //Un tableau dynamique d'Ã©lÃ©ments gÃ©nÃ©riques
 };
 
 template <typename T>
@@ -86,34 +81,34 @@ Pile<T>::Pile(): capacite_(6), nombreElements_(0) {
 
 template <typename T>
 Pile<T>::~Pile() {
-	//On libère les zones allouées dynamiquement afin d'éviter les fuites de mémoire
+	//On libÃ¨re les zones allouÃ©es dynamiquement afin d'Ã©viter les fuites de mÃ©moire
 	delete[] elements_;
 	elements_ = nullptr;
 }
 
 template <typename T>
 bool Pile<T>::empiler(const T& elem) {
-//On vérifie d'abord si la pile est pleine (pour éviter son débordement)
+//On vÃ©rifie d'abord si la pile est pleine (pour Ã©viter son dÃ©bordement)
 	if (estPleine())
 		return false;
-//Sinon, on y ajoute l'élément passé en paramètre
-//On incrémente ensuite l'indice du tableau vers la prochaine position libre (attente d'une éventuelle nouvelle valeur)
+//Sinon, on y ajoute l'Ã©lÃ©ment passÃ© en paramÃ¨tre
+//On incrÃ©mente ensuite l'indice du tableau vers la prochaine position libre (attente d'une Ã©ventuelle nouvelle valeur)
 	elements_[nombreElements_++] = elem;
 	return true;
 }
 
 template <typename T>
 bool Pile<T>::depiler(T& elem) {
-//On vérifie d'abord si la pile est vide (pas d'éléments à dépiler)
+//On vÃ©rifie d'abord si la pile est vide (pas d'Ã©lÃ©ments Ã  dÃ©piler)
 	if (estVide())
 	return false;
-/*Sinon, on y retire l'élément au sommet de la pile après l'avoir renvoyé (par paramètre pour une utilisation donnée).
-Après décrémentation de l'indice du tableau, on aurait pu tout simplement considérer que cet élément n'appartient plus à la
-pile, et que l'ajout d'un futur élément remplacera l'élément ainsi délaissé. Or une solution plus propre a été privilégiée ici 
-par la suppression des éléments dépilés (en les initialisant à leur valeur par défaut, ce qui donne par exemple une valeur nulle
-pour un double et une chaîne vide pour un string) du moment où la pile forme un tout par sa taille fixe, et puis ce choix 
-ressemble à la fonction pop_back des conteneurs de la STL; en fait, une fois cette fonctions appelée, on n'a plus accès au 
-dernier élément retiré (sauf si on l'a sauvegardé avant).*/
+/*Sinon, on y retire l'Ã©lÃ©ment au sommet de la pile aprÃ¨s l'avoir renvoyÃ© (par paramÃ¨tre pour une utilisation donnÃ©e).
+AprÃ¨s dÃ©crÃ©mentation de l'indice du tableau, on aurait pu tout simplement considÃ©rer que cet Ã©lÃ©ment n'appartient plus Ã  la
+pile, et que l'ajout d'un futur Ã©lÃ©ment remplacera l'Ã©lÃ©ment ainsi dÃ©laissÃ©. Or une solution plus propre a Ã©tÃ© privilÃ©giÃ©e ici 
+par la suppression des Ã©lÃ©ments dÃ©pilÃ©s (en les initialisant Ã  leur valeur par dÃ©faut, ce qui donne par exemple une valeur nulle
+pour un double et une chaÃ®ne vide pour un string) du moment oÃ¹ la pile forme un tout par sa taille fixe, et puis ce choix 
+ressemble Ã  la fonction pop_back des conteneurs de la STL; en fait, une fois cette fonctions appelÃ©e, on n'a plus accÃ¨s au 
+dernier Ã©lÃ©ment retirÃ© (sauf si on l'a sauvegardÃ© avant).*/
 	elem = obtenirSommet();
 	elements_[--nombreElements_] = T{};
 	return true;
@@ -132,8 +127,8 @@ bool Pile<T>::estPleine() const {
 template <typename T>
 const T& Pile<T>::obtenirSommet() const {
 //On suppose ici que la pile n'est pas vide.
-// Dans le cas contraire, il faudrait gérer des exceptions telle que throw out_of_range
-// Ou éventuellement changer le type de retour pour un pointeur afin de retourner un nullptr
+// Dans le cas contraire, il faudrait gÃ©rer des exceptions telle que throw out_of_range
+// Ou Ã©ventuellement changer le type de retour pour un pointeur afin de retourner un nullptr
 	return elements_[nombreElements_ - 1];
 }
 
